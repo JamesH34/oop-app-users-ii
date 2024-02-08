90990c26beb16028c8ec77b224fc2d58e927aa3d
James Hershey <jameshershey34@gmail.com>
2024-02-08 09:15:02 -0500

    some improvements changed staticmethod to classmethod, which makes more sense

--- User.py
+++ User.py
@@ -1,5 +1,6 @@
 class User:
     all_posts = {}
+    #this dictionary will store user data and post history
 
     def __init__(self, name, email, username):
         self.name = name 
@@ -8,27 +9,31 @@ class User:
         self.posts = []
         
 
-    def __str__(self):
-        return self.name
-    
+  
+    #make the user's name appear as a string
     def __repr__(self):
         return self.name
 
-    def password(self, secret_code):
-        return f"{self.name}'s password is {secret_code}"
+    # def password(self, secret_code):
+    #     return f"{self.name}'s password is {secret_code}"
     
     def create_post(self, post_content):
         if self in User.all_posts:
+            #look through all_posts and see if the user exists already
             User.all_posts[self].append(post_content)
         else:
             User.all_posts[self] = [post_content]
+            #create new entry with self as key and post_content as value
         print(f"{self.name} created a new post: {post_content}")
 
-    @staticmethod
-    def get_all_posts():
+    
+    @classmethod
+    #use classmethod because  all_posts is a class attribute
+    def get_all_posts(cls):
         return User.all_posts
     
 for user, posts in User.get_all_posts().items():
+    # gather all the posts from get_all_posts
     print(f"{user.name}'s posts:")
     for post in posts:
         print(post)
@@ -39,10 +44,10 @@ user1 = User("Kevin", "Kevin@gmail.com", "kmcalister")
 user2 = User("George", "George@yahoo.com", "gcostanza")
 
 user1.create_post("Keep the change")
-user1.create_post("I'm dreaming of a white christmas")
+user1.create_post("I love cheese pizza")
 
 
-user2.create_post("I wish i wasn't bald")
+user2.create_post("I'm moving to Del Boca Vista")
 user2.create_post("I love the Yankees")
 
 
